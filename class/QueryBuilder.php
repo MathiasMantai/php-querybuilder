71b623ed2ba0141e924f00a14dc0b09825452b22
MathiasMantai <mmantaibusiness@gmail.com>
2022-11-09 11:00:25 +0100

    - join methods - lineBreak methods

--- class/QueryBuilder.php
+++ class/QueryBuilder.php
@@ -5,6 +5,8 @@
  * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
  */
+
+
 class QueryBuilder {
     
     private string $query;
@@ -20,7 +22,9 @@ class QueryBuilder {
      */
     public function select(array $fields, array $alias = null) : void {
         $this->query .= "SELECT ";
+
         $count = count($fields);
+
         for($i = 0; $i < $count; $i++) {
 
             $this->query .= $fields[$i];
@@ -32,37 +36,67 @@ class QueryBuilder {
             if($i < $count-1) {
                 $this->query .= ", ";
             }
-        }       
+        } 
     }
 
     public function from(string $table, string $alias = null) : void {
         $this->query .= " FROM " . $table;
+
         if($alias != null) {
             $this->query .= " AS " . $alias;
-        } 
+        }
     }
 
     public function innerJoin(string $table, string $alias = null, string $on = null) : void {
+        $this->query .= " INNER JOIN " . $table;
 
+        if($alias != null) {
+            $this->query .= " AS " . $alias;
+        }
+
+        if($on != null) {
+            $this->query .= " ON " . $on;
+        }
     }
 
     public function leftJoin(string $table, string $alias = null, string $on = null) : void {
-        
+        $this->query .= " LEFT JOIN " . $table;
+
+        if($alias != null) {
+            $this->query .= " AS " . $alias;
+        }
+
+        if($on != null) {
+            $this->query .= " ON " . $on;
+        }
     }
 
     public function rightJoin(string $table, string $alias = null, string $on = null) : void {
-        
+        $this->query .= " RIGHT JOIN " . $table;
+
+        if($alias != null) {
+            $this->query .= " AS " . $alias;
+        }
+
+        if($on != null) {
+            $this->query .= " ON " . $on;
+        }
     }
 
     public function fullJoin(string $table, string $alias = null) : void {
-        
+        $this->query .= " FULL JOIN " . $table;
+
+        if($alias != null) {
+            $this->query .= " AS " . $alias;
+        }
     }
 
-    public function naturalJoin(string $table, string $alias = null) : void {
-        
+    public function naturalJoin(string $table) : void {
+        $this->query .= " NATURAL JOIN " . $table;
     }
 
     public function where(string $field, string $operator = null, string $value = null) : void {
+
         $this->query .= " WHERE " . $field;
 
         if($operator != null) {
@@ -87,5 +121,13 @@ class QueryBuilder {
      */
     public function getQuery() : string {
         return $this->query;
+    }
+
+    public function lineBreak() : void {
+        $this->query .= "\n";
+    }
+
+    public function lineBreakHTML() : void {
+        $this->query .= "<br>";
     }
 }
