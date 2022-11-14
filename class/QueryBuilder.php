918d0c9d9598dd98bc558dbda9c2c0ae80a349b1
Mantai59 <mmantaibusiness@gmail.com>
2022-11-14 21:25:53 +0100

    method documentation

--- class/QueryBuilder.php
+++ class/QueryBuilder.php
@@ -17,6 +17,9 @@ class QueryBuilder {
     }
 
     /**
+     * @param array $fields
+     * @param array $ailas
+     * @return void
      * $fields = ["field1", "field2"]
      * $alias = ["f1", "f2"]
      * will result in the following query: SELECT field1 AS f1, field2 AS f2
@@ -41,9 +44,21 @@ class QueryBuilder {
     }
 
     /**
-     *  @return void
+     * @param string $table
+     * @param string $where
+     * @return void
+     */    
+    public function selectAll(string $table, string $where = null): void {
+        $this->query .= "SELECT * FROM " . $table;
+        if($where != null) {
+            $this->query .= " WHERE " . $where;
+        }
+    }
+
+    /**
      *  @param string $table
      *  @param string $alias
+     *  @return void
      */
     public function from(string $table, string $alias = null) : void {
         $this->query .= " FROM " . $table;
@@ -54,10 +69,10 @@ class QueryBuilder {
     }
 
     /**
-     *  @return void
      *  @param string $table
      *  @param string $alias
      *  @param string $on
+     *  @return void
      */
     public function innerJoin(string $table, string $alias = null, string $on = null) : void {
         $this->query .= " INNER JOIN " . $table;
@@ -71,6 +86,12 @@ class QueryBuilder {
         }
     }
 
+    /**
+     * @param string $table
+     * @param string $alias
+     * @param string $on
+     * @return void
+     */
     public function leftJoin(string $table, string $alias = null, string $on = null) : void {
         $this->query .= " LEFT JOIN " . $table;
 
@@ -83,6 +104,12 @@ class QueryBuilder {
         }
     }
 
+    /**
+     * @param string $table
+     * @param string $alias
+     * @param string $on
+     * @return void
+     */
     public function rightJoin(string $table, string $alias = null, string $on = null) : void {
         $this->query .= " RIGHT JOIN " . $table;
 
@@ -95,6 +122,11 @@ class QueryBuilder {
         }
     }
 
+    /**
+     * @param string $table
+     * @param string $alias
+     * @return void
+     */
     public function fullJoin(string $table, string $alias = null) : void {
         $this->query .= " FULL JOIN " . $table;
 
@@ -103,10 +135,21 @@ class QueryBuilder {
         }
     }
 
+    /**
+     * @param string $table
+     * @return void
+     */
     public function naturalJoin(string $table) : void {
         $this->query .= " NATURAL JOIN " . $table;
     }
 
+
+    /**
+     * @param string $field
+     * @param string $operator
+     * @param string $value
+     * @return void
+     */
     public function where(string $field, string $operator = null, string $value = null) : void {
 
         $this->query .= " WHERE " . $field;
@@ -120,25 +163,40 @@ class QueryBuilder {
         }
     }
 
+    /**
+     * @param string $fields
+     * @return void
+     */
     public function orderBy(string $fields) : void {
         $this->query .= " ORDER BY " . $fields;
     }
 
+    /**
+     * @param string $fields
+     * @return void
+     */
     public function groupBy(string $fields) : void {
         $this->query .= " GROUP BY " . $fields;
     }
 
     /**
      * Return the query as a string
+     * @return string
      */
     public function getQuery() : string {
         return $this->query;
     }
 
+    /**
+     * @return void
+     */
     public function lineBreak() : void {
         $this->query .= "\n";
     }
 
+    /**
+     * @return void
+     */
     public function lineBreakHTML() : void {
         $this->query .= "<br>";
     }
