c77f90311725206b5df80986ff9272b4a5825323
mathiasmantai <mmantaibusiness@gmail.com>
2023-09-01 14:11:42 +0200

    chore: added a trait for utility functions for query builder

--- class/QueryBuilder.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace M2\QueryBuilder;
-
-interface QueryBuilderInterface 
-{
-    //WIP
-}
---
+++ class/QueryBuilderUtil.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace M2\QueryBuilder;
+
+trait QueryBuilderUtil
+{
+    //WIP
+    public function formatString(string $value)
+    {
+        return "'" . $value . "'";
+    }
+}
