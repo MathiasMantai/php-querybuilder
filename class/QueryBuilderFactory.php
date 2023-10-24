2d4639708e69e11fc0f8e85573918eb6939fda22
mathiasmantai <mmantaibusiness@gmail.com>
2023-10-24 11:58:47 +0000

    chore: adding default return to factory class

--- class/QueryBuilderFactory.php
+++ class/QueryBuilderFactory.php
@@ -3,16 +3,18 @@
 namespace MMantai\QueryBuilder;
 
 use MMantai\QueryBuilder\MySQLQueryBuilder;
-
+use MMantai\QueryBuilder\PostgreSQLQueryBuilder;
 
 class QueryBuilderFactory
 {
-    public static function create($type)
+    public static function create($type): MySQLQueryBuilder | PostgreSQLQueryBuilder | null
     {
         switch($type)
         {
             case "mysql": return new MySQLQueryBuilder();
             case "pgsql": return new PostgreSQLQueryBuilder();
         }
+
+        return null;
     }
 }
