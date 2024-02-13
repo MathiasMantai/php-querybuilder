4ad961544e6e3fdad2cf3cfd03841e8822b6d5b4
mathiasmantai <mmantaibusiness@gmail.com>
2024-02-13 15:04:12 +0000

    chore: create function to create a comma separated list of bind params

--- class/QueryBuilderUtil.php
+++ class/QueryBuilderUtil.php
@@ -5,8 +5,13 @@ namespace Mmantai\QueryBuilder;
 trait QueryBuilderUtil
 {
     //WIP
-    public function formatString(string $value)
+    public function formatString(string $string)
     {
-        return "'" . $value . "'";
+        return "'{$string}'"; 
+    }
+    
+    public function createBindParamList(int $valueCnt)
+    {
+        return implode(", ", array_fill(0,$valueCnt, '?'));
     }
 }
