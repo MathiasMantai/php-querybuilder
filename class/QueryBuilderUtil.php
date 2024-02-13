44c08aacbd1505643d4b74d7858139fcddbf3c48
mathiasmantai <mmantaibusiness@gmail.com>
2024-02-13 15:15:34 +0000

    chore: created methods to check first dimension of an array and whether all dimensions of an array match

--- class/QueryBuilderUtil.php
+++ class/QueryBuilderUtil.php
@@ -14,4 +14,27 @@ trait QueryBuilderUtil
     {
         return implode(", ", array_fill(0,$valueCnt, '?'));
     }
+
+    public function getFirstArrayDimension(array $arr)
+    {
+        return is_array($arr[0]);
+    }
+    
+    public function arrayDimensionsMatch(array $arr)
+    {
+        //get first dimension
+        $dim = is_array($arr[0]) ? 2 : 1;
+        
+        array_shift($arr);
+
+        foreach($arr as $arrPart)
+        {
+            if(!is_array($arrPart))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
